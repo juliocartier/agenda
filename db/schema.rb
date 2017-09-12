8a0cb2b36018a07e74628d0f69a7516e39f0eab6
Julio Cartier <juliocartier@gmail.com>
2017-09-12 13:54:57 -0300

    Alteração no banco

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909010310) do
+ActiveRecord::Schema.define(version: 20170905124717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,21 +20,6 @@ ActiveRecord::Schema.define(version: 20170909010310) do
     t.string "endereco"
     t.string "email"
     t.date "dataNascimento"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "rooms", force: :cascade do |t|
-    t.string "title"
-    t.string "location"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "telefones", force: :cascade do |t|
-    t.string "nome"
-    t.string "numero"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
